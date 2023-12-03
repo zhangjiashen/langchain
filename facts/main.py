6605705fc764dea354df435209302911c7d5e7b6
Jiashen Zhang <jiashenzzzzz@gmail.com>
2023-12-02 18:12:01 -0800

    Custom document retriever

--- facts/main.py
+++ facts/main.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,6 +19,16 @@ docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-for doc in docs:
-    print(doc.page_content)
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search(
+    "What is an interesting fact about the English language?"
+)
+
+for result in results:
     print("\n")
+    print(result.page_content)
